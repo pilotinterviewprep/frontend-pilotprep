d028519de8186bdeec78d8606615c5b2609e57da
codemine24 <codemine24@gmail.com>
2025-01-29 10:09:12 +0600

    auth removed from private group

---
+++ src/layouts/private-layout.tsx
@@ -0,0 +1,50 @@
+import 'src/global.css';
+
+// ----------------------------------------------------------------------
+
+
+import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
+
+import ReduxProvider from 'src/providers/redux-provider';
+import { schemeConfig } from 'src/theme/scheme-config';
+import { ThemeProvider } from 'src/theme/theme-provider';
+
+import { MotionLazy } from 'src/components/animate/motion-lazy';
+import { ProgressBar } from 'src/components/progress-bar';
+import { defaultSettings, SettingsDrawer, SettingsProvider } from 'src/components/settings';
+
+import { AuthProvider } from 'src/auth/context/jwt';
+import { AuthSplitLayout } from 'src/layouts/auth-split';
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const PrivateLayout = ({ children }: Props) => {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <InitColorSchemeScript
+          defaultMode={schemeConfig.defaultMode}
+          modeStorageKey={schemeConfig.modeStorageKey}
+        />
+
+        <ReduxProvider>
+          <AuthProvider>
+            <SettingsProvider settings={defaultSettings}>
+              <ThemeProvider>
+                <MotionLazy>
+                  <ProgressBar />
+                  <SettingsDrawer />
+                  {children}
+                </MotionLazy>
+              </ThemeProvider>
+            </SettingsProvider>
+          </AuthProvider>
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+};
